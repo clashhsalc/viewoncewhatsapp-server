4159dd9947e2fc70ff9a6a9b7d41ea861744852c
aryan <83342701+clashhsalc@users.noreply.github.com>
2024-12-16 18:42:35 +0530

    update

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send('["WAWebE2EProtoParser","parseMsgProto"]');
 }
